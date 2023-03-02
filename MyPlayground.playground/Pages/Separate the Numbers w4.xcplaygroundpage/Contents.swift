2a541963c9c2d31179cafe596f39327b080318f0
Ho Trung Toan <trungtoan1093@gmail.com>
2023-03-03 06:29:21 +0700

    update w4

--- MyPlayground.playground/Pages/Separate the Numbers w4.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Separate the Numbers w4.xcplaygroundpage/Contents.swift
@@ -6,24 +6,53 @@ var greeting = "Hello, playground"
 
 //: [Next](@next)
 func separateNumbers(s: String) -> Void {
-    // Write your code here
-    var numbers: [Int] = []
-    s.forEach {
-        if let item = Int(String($0)) {
-            numbers.append(item)
+
+}
+
+func check(range: Int, s: String) {
+    guard range < s.count else { return }
+    
+//    print("range: \(range), s:\(s)")
+    
+    let value = s.prefix(range)
+    let nextstart = s.index(s.startIndex, offsetBy: range)
+    let nextend = s.index(nextstart, offsetBy: range)
+    let nextRange = nextstart..<nextend
+    let nextValue = s[nextRange]
+    
+//    if value + 1 == nextValue {
+//        let count = String(nextValue).count
+//        check(range: count, s: String(s.dropFirst(count)))
+//    } else if value + 1 == nextValue2 {
+//        let count = String(nextValue2).count
+//        check(range: count, s: String(s.dropFirst(count)))
+//    } else {
+////        ==> fail
+//    }
+    
+    
+    
+    if (Int(value) ?? 0) + 1 == (Int(nextValue) ?? 0) {
+        print("value: \(nextValue)")
+        check(range: nextValue.count, s: String(s.dropFirst(nextValue.count)))
+//        ==> pass
+    } else if Int(value.dropLast()) ?? 0 == 9 {
+        let nextend2 = s.index(nextstart, offsetBy: range+1)
+        let nextRange2 = nextstart..<nextend2
+        let nextValue2 = s[nextRange2]
+        if (Int(value) ?? 0) + 1 == (Int(nextValue2) ?? 0) {
+//            ==> pass
+
+            check(range: nextValue2.count, s: String(s.dropFirst(nextValue2.count)))
+        } else {
+            
         }
-    }
-    var currentValue = numbers.first!
-    var currentIndex: [Int] = []
-    var count: Int {
-        return currentIndex.count
-    }
-    for i in 0..<numbers.count {
-        if currentIndex.contains(i) { continue }
-        
+    } else {
         
     }
 }
+
+check(range: 1, s: "12345")
 
 separateNumbers(s: "123")
 //"1234" => "1" "2" "3" "4"
